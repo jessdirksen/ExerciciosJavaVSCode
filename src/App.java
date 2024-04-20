00d73310dd81935ca83e21bed645ae49fdfacd02
jessdirksen <jehdirksen@gmail.com>
2024-04-20 19:19:29 -0300

    hello word teste

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JOptionPane.showMessageDialog(null, "Hello, World!");
+        System.out.println("Hello, World!");
+    }
+}
